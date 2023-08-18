e4f047e1d921bd6b5b64e6ae080aec3e07e1ddf9
CASEY GUNGEON <bound2theground@gmail.com>
2023-08-18 15:36:02 -0400

    convert OvermapShuttle.js to .tsx

--- tgui/packages/tgui/interfaces/OvermapShuttle.tsx
+++ tgui/packages/tgui/interfaces/OvermapShuttle.tsx
@@ -1,11 +1,58 @@
+import { classes } from 'common/react';
 import { useBackend, useLocalState } from '../backend';
 import { Tabs, Box, Button, Dropdown, Flex, Icon, LabeledList, Modal, Section } from '../components';
 import { Window } from '../layouts';
 
 // act('action', { param1: 'value', })
 
+type ShuttleData = {
+  overmapView: boolean;
+  status: string;
+  // GENERAL
+  shields: number;
+  position_x: number;
+  position_y: number;
+  commsListen: boolean;
+  commsBroadcast: boolean;
+  // ENGINES
+  engines: Engine[];
+  // HELM
+  destination_x: number;
+  destination_y: number;
+  speed: number;
+  impulse: number;
+  topSpeed: number;
+  currentCommand: string;
+  padControl: boolean;
+  // SENSORS
+  sensorTargets: Target[];
+  // TARGET
+  lockedTarget: Target;
+  lockStatus: number;
+  scanInfo: string;
+  // DOCK
+  docks: Dock[];
+  freeformDocks: FreeDock[];
+}
+
+type Engine = {
+
+}
+
+type Target = {
+
+}
+
+type Dock = {
+
+}
+
+type FreeDock = {
+
+}
+
 export const OvermapShuttle = (props, context) => {
-  const { act, data } = useBackend(context);
+  const { act, data } = useBackend<ShuttleData>(context);
   const lockStates = {
     None: Symbol('none'),
     Locking: Symbol('locking'),
@@ -85,13 +132,16 @@ export const OvermapShuttle = (props, context) => {
             position_x={position_x}
             position_y={position_y} />
         )}
+        {tab === 2 && (
+          <OvermapShuttleEngines />
+        )}
       </Window.Content>
     </Window>
   );
 };
 
 export const OvermapShuttleGeneral = (props, context) => {
-  const { act, data } = useBackend(context);
+  const { act, data } = useBackend<ShuttleData>(context);
   const {
     position_x,
     position_y,
@@ -101,6 +151,19 @@ export const OvermapShuttleGeneral = (props, context) => {
       <Box>
         <b>POSITION: ( {position_x}, {position_y} ) </b>
       </Box>
+    </Section>
+  );
+};
+
+export const OvermapShuttleEngines = (props, context) => {
+  const { act, data } = useBackend(context);
+  return (
+    <Section>
+      <Button
+        ml={1}
+        color="yellow"
+        Content="Engines On"
+        onClick={() => act("engines_on")} />
     </Section>
   );
 };
