1a01a6e46795ed4d8f1a952f5abe874e46056eae
alos <90494794+whataboutism-alos@users.noreply.github.com>
2024-01-11 15:00:15 -0700

    Update Vending.tsx
    Adds /tg/UI capability to see vendor categories.

--- tgui/packages/tgui/interfaces/Vending.tsx
+++ tgui/packages/tgui/interfaces/Vending.tsx
@@ -1,6 +1,6 @@
 import { classes } from 'common/react';
 import { useBackend } from '../backend';
-import { Box, Button, Section, Table } from '../components';
+import { Box, Button, Icon, LabeledList, NoticeBox, Section, Stack, Table } from '../components';
 import { Window } from '../layouts';
 
 type VendingData = {
@@ -15,7 +15,12 @@ type VendingData = {
   extended_inventory: boolean;
   access: boolean;
   vending_machine_input: CustomInput[];
+  categories: Record<string, Category>;
 }
+
+type Category = {
+  icon: string;
+};
 
 type ProductRecord = {
   path: string;
@@ -23,6 +28,7 @@ type ProductRecord = {
   price: number;
   max_amount: number;
   ref: string;
+  category: string;
 }
 
 type CoinRecord = {
@@ -52,8 +58,10 @@ type UserData = {
 
 type StockItem = {
   name: string;
+  path: string;
   amount: number;
-}
+  customization_flags: boolean;
+};
 
 type CustomInput = {
   name: string;
@@ -160,6 +168,10 @@ export const Vending = (props, context) => {
     hidden_records = [],
     stock,
   } = data;
+  const [selectedCategory, setSelectedCategory] = useState(
+    Object.keys(data.categories)[0],
+  );
+  
   let inventory;
   let custom = false;
   if (data.vending_machine_input) {
@@ -180,42 +192,311 @@ export const Vending = (props, context) => {
   }
   // Just in case we still have undefined values in the list
   inventory = inventory.filter(item => !!item);
-  return (
+
+  const filteredCategories = Object.fromEntries(
+    Object.entries(data.categories).filter(([categoryName]) => {
+      return inventory.find((product) => {
+        if ('category' in product) {
+          return product.category === categoryName;
+        } else {
+          return false;
+        }
+      });
+    }),
+  );
+  
+ return (
     <Window
       title="Vending Machine"
       width={450}
       height={600}>
       <Window.Content scrollable>
-        {!!onstation && (
-          <Section title="User">
-            {user && (
-              <Box>
-                Welcome, <b>{user.name}</b>,
-                {' '}
-                <b>{user.job || 'Unemployed'}</b>!
-                <br />
-                Your balance is <b>{user.cash} credits</b>.
-              </Box>
-            ) || (
-              <Box color="light-grey">
-                No registered ID card!<br />
-                Please contact your local HoP!
-              </Box>
-            )}
-          </Section>
-        )}
-        <Section title="Products">
-          <Table>
-            {inventory.map(product => (
-              <VendingRow
-                key={product.name}
-                custom={custom}
-                product={product}
-                productStock={stock[product.name]} />
-            ))}
-          </Table>
-        </Section>
+        <Stack fill vertical>
+          {!!onstation && (
+            <Stack.Item>
+              <UserDetails />
+            </Stack.Item>
+          )}
+          <Stack.Item grow>
+            <ProductDisplay
+              custom={custom}
+              inventory={inventory}
+              selectedCategory={selectedCategory}
+            />
+          </Stack.Item>
+
+          {Object.keys(filteredCategories).length > 1 && (
+            <Stack.Item>
+              <CategorySelector
+                categories={filteredCategories}
+                selectedCategory={selectedCategory!}
+                onSelect={setSelectedCategory}
+              />
+            </Stack.Item>
+          )}
+        </Stack>
       </Window.Content>
     </Window>
+  );
+};
+
+/** Displays user details if an ID is present and the user is on the station */
+export const UserDetails = (props) => {
+  const { data } = useBackend<VendingData>();
+  const { user } = data;
+
+  if (!user) {
+    return (
+      <NoticeBox>No registered ID card! Please contact your local Personnel Office!</NoticeBox>
+    );
+  } else {
+    return (
+      <Section>
+        <Stack>
+          <Stack.Item>
+            <Icon name="id-card" size={3} mr={1} />
+          </Stack.Item>
+          <Stack.Item>
+            <LabeledList>
+              <LabeledList.Item label="User">{user.name}</LabeledList.Item>
+              <LabeledList.Item label="Occupation">
+                {user.job || 'Unemployed'}                
+              </LabeledList.Item>
+            </LabeledList>
+          </Stack.Item>
+        </Stack>
+      </Section>
+    );
+  }
+};
+
+/** Displays  products in a section, with user balance at top */
+const ProductDisplay = (props: {
+  custom: boolean;
+  selectedCategory: string | null;
+  inventory: (ProductRecord | CustomInput)[];
+}) => {
+  const { data } = useBackend<VendingData>();
+  const { custom, inventory, selectedCategory } = props;
+  const {
+    stock,
+    onstation,
+    user,
+    displayed_currency_icon,
+    displayed_currency_name,
+  } = data;
+
+  return (
+    <Section
+      fill
+      scrollable
+      title="Products"
+      buttons={
+        !!onstation &&
+        user && (
+          <Box fontSize="16px" color="green">
+            {(user && user.cash) || 0}
+            {displayed_currency_name}{' '}
+            <Icon name={displayed_currency_icon} color="gold" />
+          </Box>
+        )
+      }
+    >
+      <Table>
+        {inventory
+          .filter((product) => {
+            if ('category' in product) {
+              return product.category === selectedCategory;
+            } else {
+              return true;
+            }
+          })
+          .map((product) => (
+            <VendingRow
+              key={product.path}
+              custom={custom}
+              product={product}
+              productStock={stock[product.path]}
+            />
+          ))}
+      </Table>
+    </Section>
+  );
+};
+
+/** An individual listing for an item.
+ * Uses a table layout. Labeledlist might be better,
+ * but you cannot use item icons as labels currently.
+ */
+const VendingRow = (props) => {
+  const { data } = useBackend<VendingData>();
+  const { custom, product, productStock } = props;
+  const { access, department, jobDiscount, onstation, user } = data;
+  const free = !onstation || product.price === 0;
+  const discount = !product.premium && department === user?.department;
+  const remaining = custom ? product.amount : productStock.amount;
+  const redPrice = Math.round(product.price * jobDiscount);
+  const disabled =
+    remaining === 0 ||
+    (onstation && !user) ||
+    (onstation &&
+      !access &&
+      (discount ? redPrice : product.price) > user?.cash);
+
+  /* POLYCHROME - DISABLED. TODO: GET WORKING.
+  ...<Table.Cell bold>{capitalizeAll(product.name)}</Table.Cell>
+      <Table.Cell>
+        {!!productStock?.colorable && (<ProductColorSelect disabled={disabled} product={product} />)}
+      </Table.Cell>
+      <Table.Cell collapsing textAlign="right">...
+  */
+  return (
+    <Table.Row>
+      <Table.Cell collapsing>
+        <ProductImage product={product} />
+      </Table.Cell>
+      <Table.Cell bold>{capitalizeAll(product.name)}</Table.Cell>
+      <Table.Cell collapsing textAlign="right">
+        <ProductStock custom={custom} product={product} remaining={remaining} />
+      </Table.Cell>
+      <Table.Cell collapsing textAlign="center">
+        <ProductButton
+          custom={custom}
+          disabled={disabled}
+          discount={discount}
+          free={free}
+          product={product}
+          redPrice={redPrice}
+        />
+      </Table.Cell>
+    </Table.Row>
+  );
+};
+
+/** Displays the product image. Displays a default if there is none. */
+const ProductImage = (props) => {
+  const { product } = props;
+
+  return product.img ? (
+    <img
+      src={`data:image/jpeg;base64,${product.img}`}
+      style={{
+        verticalAlign: 'middle',
+      }}
+    />
+  ) : (
+    <span
+      className={classes(['vending32x32', product.path])}
+      style={{
+        verticalAlign: 'middle',
+      }}
+    />
+  );
+};
+
+/** POLYCHROME - DISABLED. TODO: GET WORKING.
+ * In the case of customizable items, ie: shoes,
+ * this displays a color wheel button that opens another window.
+ */
+/*
+const ProductColorSelect = (props) => {
+  const { act } = useBackend<VendingData>();
+  const { disabled, product } = props;
+
+  return (
+    <Button
+      icon="palette"
+      tooltip="Change color"
+      disabled={disabled}
+      onClick={() => act('select_colors', { ref: product.ref })}
+    />
+  );
+};
+*/
+
+/** Displays a colored indicator for remaining stock */
+const ProductStock = (props) => {
+  const { custom, product, remaining } = props;
+
+  return (
+    <Box
+      color={
+        (remaining <= 0 && 'bad') ||
+        (!custom && remaining <= product.max_amount / 2 && 'average') ||
+        'good'
+      }
+    >
+      {remaining} left
+    </Box>
+  );
+};
+
+/** The main button to purchase an item. */
+const ProductButton = (props) => {
+  const { act, data } = useBackend<VendingData>();
+  const { access, displayed_currency_name } = data;
+  const { custom, discount, disabled, free, product, redPrice } = props;
+  const customPrice = access ? 'FREE' : product.price;
+  let standardPrice = product.price;
+  if (free) {
+    standardPrice = 'FREE';
+  } else if (discount) {
+    standardPrice = redPrice;
+  }
+  return custom ? (
+    <Button
+      fluid
+      disabled={disabled}
+      onClick={() =>
+        act('dispense', {
+          item: product.path,
+        })
+      }
+    >
+      {customPrice}
+      {!access && displayed_currency_name}
+    </Button>
+  ) : (
+    <Button
+      fluid
+      disabled={disabled}
+      onClick={() =>
+        act('vend', {
+          ref: product.ref,
+        })
+      }
+    >
+      {standardPrice}
+      {!free && displayed_currency_name}
+    </Button>
+  );
+};
+
+const CATEGORY_COLORS = {
+  Contraband: 'theme-operative',
+  Premium: 'yellow',
+};
+
+const CategorySelector = (props: {
+  categories: Record<string, Category>;
+  selectedCategory: string;
+  onSelect: (category: string) => void;
+}) => {
+  const { categories, selectedCategory, onSelect } = props;
+
+  return (
+    <Section>
+      {Object.entries(categories).map(([name, category]) => (
+        <Button
+          key={name}
+          selected={name === selectedCategory}
+          color={CATEGORY_COLORS[name]}
+          icon={category.icon}
+          onClick={() => onSelect(name)}
+        >
+          {name}
+        </Button>
+      ))}
+    </Section>
   );
 };
